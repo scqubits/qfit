be59c86482f0f6e67e0b327579edf7448db26b05
Danyang Chen <dychen1999@foxmail.com>
2023-07-13 19:38:04 -0500

    init commit of the numerical spectrum model

---
+++ qfit/models/numerical_spectrum_data.py
@@ -0,0 +1,17 @@
+# extracted_data.py
+#
+# This file is part of qfit.
+#
+#    Copyright (c) 2020, Jens Koch
+#    All rights reserved.
+#
+#    This source code is licensed under the BSD-style license found in the
+#    LICENSE file in the root directory of this source tree.
+############################################################################
+
+
+from typing import Callable, Optional, Union
+
+import numpy as np
+import qfit.io_utils.file_io_serializers as serializers
+from qfit.core.data_structures import Database
