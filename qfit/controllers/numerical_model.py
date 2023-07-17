1ed7b916db0410f970ce02f701df30db8c1432af
Tianpu Zhao <pacosynthesis@gmail.com>
2023-07-17 14:20:41 -0500

    Implemented methods for generating QuantumModelParameterSets. Changed some method and class names.

--- qfit/controllers/numerical_model.py
+++ qfit/controllers/numerical_model.py
@@ -1,18 +1,22 @@
 import numpy as np
 
-import scqubits as scq
+from typing import overload
 
 from scqubits.core.hilbert_space import HilbertSpace
 from scqubits.core.param_sweep import ParameterSweep
 from scqubits.core.qubit_base import QuantumSystem
 
 from typing import Dict, List, Tuple, Union
+from qfit.models.parameter_settings import ParameterType
 
 from qfit.models.quantum_model_parameters import (
     QuantumModelParameter,
+    QuantumModelSliderParameter,
     QuantumModelParameterSet,
 )
 from qfit.models.numerical_spectrum_data import SpectrumData
+
+from qfit.models.parameter_settings import QSYS_PARAM_NAMES, DEFAULT_PARAM_MINMAX
 
 
 class QuantumModel:
@@ -33,30 +37,98 @@ class QuantumModel:
     ):
         super().__init__()
         self.hilbertspace: HilbertSpace = hilbertspace
-        self.parameter_set: QuantumModelParameterSet = self.getParamNames(
+        self.parameter_set: QuantumModelParameterSet = self.generateParameterSets(
             self.hilbertspace
         )
         self.sweep = self._generateParameterSweep()
 
+    @overload
+    def generateParameterSets(
+        hilbertspace: HilbertSpace,
+        included_parameter_type: List[ParameterType],
+    ) -> QuantumModelParameterSet:
+        ...
+
+    @overload
+    def generateParameterSets(
+        hilbertspace: HilbertSpace,
+        excluded_parameter_type: List[ParameterType],
+    ) -> QuantumModelParameterSet:
+        ...
+
     @staticmethod
-    def getParamNames(hilbertspace: HilbertSpace) -> QuantumModelParameterSet:
+    def generateParameterSets(
+        hilbertspace: HilbertSpace,
+        included_parameter_type: Union[List[ParameterType], None] = None,
+        excluded_parameter_type: Union[List[ParameterType], None] = None,
+    ) -> QuantumModelParameterSet:
         """
         Get the names of all parameters (excluding offset charges and external fluxes) in the HilbertSpace object.
         The returned dictionary has subsystem id strings as keys and dictionaries of different types of parameters
         as values. For example, if the HilbertSpace object contains two capacitively coupled transmon qubits,
-        the returned dictionary will look like:
+        the returned QuantumModelParameterSet object will have a parameter object (a dictionary):
 
-        {"tmon1": {"circuit elements": ["EJ", "EC"], "cutoffs": ["ncut"], "truncated dimensions": ["truncated_dim"]},
-        "tmon2": {"circuit elements": ["EJ", "EC"], "cutoffs": ["ncut"], "truncated dimensions": ["truncated_dim"]},
-        "coupling": {"coupling coefficients": ["g1"]}
+        {tmon1: {"EJ": ["EJ"], "EC": ["EC"], "cutoffs": ["ncut"], "truncated_dim": ["truncated_dim"]},
+        tmon2: {"EJ": ["EJ"], "EC": ["EC"], "cutoffs": ["ncut"], "truncated_dim": ["truncated_dim"]},
+        hilbertspace: {"interaction_strength": ["g1"]}
         }
 
         Returns
         -------
-            Names of parameters in the HilbertSpace object
+            A QuantumModelParameterSet object that stores all the parameters in the HilbertSpace object.
         """
-
-        return
+        # only one of the included_parameter_type or excluded_parameter_type can be specified
+        if included_parameter_type is not None and excluded_parameter_type is not None:
+            raise ValueError(
+                "Only one of included_parameter_type or excluded_parameter_type can be specified."
+            )
+        # first, obtain all the parameters in the subsystems of the HilbertSpace object
+        parameter_set = QuantumModelParameterSet()
+        subsystems = hilbertspace.subsystem_list
+        for subsystem in subsystems:
+            # obtain the parameter names in the subsystem
+            # first identify the type of the subsystem
+            subsystem_type = subsystem.__class__
+            # then get the parameters in the subsystem
+            parameters = QSYS_PARAM_NAMES[subsystem_type]
+            # add the parameter names to the parameter set
+            # parameters is a dictionary with parameter types as keys and lists of parameter names as values
+            for parameter_type, parameter_names in parameters.items():
+                # check if the parameter type is included or excluded
+                if (
+                    (included_parameter_type is not None)
+                    and (parameter_type not in included_parameter_type)
+                ) or (
+                    (excluded_parameter_type is not None)
+                    and (parameter_type in excluded_parameter_type)
+                ):
+                    continue
+                for parameter_name in parameter_names:
+                    parameter_set.add_parameter(
+                        parameter_name,
+                        subsystem,
+                        DEFAULT_PARAM_MINMAX[parameter_type],
+                        parameter_type,
+                    )
+        # then add interaction strengths to the parameter set
+        if (
+            (included_parameter_type is not None)
+            and ("interaction_strength" not in included_parameter_type)
+        ) or (
+            (excluded_parameter_type is not None)
+            and ("interaction_strength" in excluded_parameter_type)
+        ):
+            return parameter_set
+        else:
+            interactions = hilbertspace.interaction_list
+            for interaction_term_index in range(len(interactions)):
+                parameter_set.add_parameter(
+                    f"g{interaction_term_index+1}",
+                    hilbertspace,
+                    DEFAULT_PARAM_MINMAX["interaction_strength"],
+                    "interaction_strength",
+                )
+            return parameter_set
 
     @staticmethod
     def _map1D(x: float, coeffs, biases) -> Tuple[QuantumModelParameter, ...]:
@@ -96,7 +168,7 @@ class QuantumModel:
 
         Parameters
         ----------
-        parameter: QuantumSystemParameter
+        parameter: QuantumModelParameter
         """
 
         pass
@@ -110,7 +182,7 @@ class QuantumModel:
 
         Parameters
         ----------
-        parameter_set: QuantumSystemParameterSet
+        parameter_set: QuantumModelParameterSet
         """
 
         pass
---
+++ qfit/models/parameter_settings.py
@@ -0,0 +1,96 @@
+from scqubits import (
+    Transmon,
+    TunableTransmon,
+    Fluxonium,
+    FluxQubit,
+    Cos2PhiQubit,
+    Oscillator,
+    KerrOscillator,
+)
+
+from typing_extensions import Literal
+
+ParameterType = Literal[
+    "EC",
+    "EJ",
+    "EL",
+    "E_osc",
+    "l_osc",
+    "ng",
+    "flux",
+    "cutoff",
+    "interaction_strength",
+    "truncated_dim",
+]
+
+# default ranges for different types of parameters
+DEFAULT_PARAM_MINMAX = {
+    "EJ": {"min": 1e-5, "max": 70.0},
+    "EC": {"min": 1e-5, "max": 10.0},
+    "EL": {"min": 1e-5, "max": 10.0},
+    "E_osc": {"min": 1e-5, "max": 20.0},
+    "l_osc": {"min": 1e-5, "max": 10.0},
+    "K": {"min": 1e-5, "max": 10.0},
+    "disorder": {"min": -1 + 1e-5, "max": 1 - 1e-5},
+    "ng": {"min": 0.0, "max": 1.0},
+    "flux": {"min": 0.0, "max": 1.0},
+    "cutoff": {"min": 10, "max": 50},
+    "interaction_strength": {"min": -10.0, "max": 10.0},
+    "truncated_dim": {"min": 1, "max": 30},
+}
+
+QSYS_PARAM_NAMES = {
+    Transmon: {
+        "EJ": ["EJ"],
+        "EC": ["EC"],
+        "ng": ["ng"],
+        "cutoffs": ["ncut"],
+        "truncated_dim": ["truncated_dim"],
+    },
+    TunableTransmon: {
+        "EJ": ["EJmax"],
+        "EC": ["EC"],
+        "disorder": ["d"],
+        "ng": ["ng"],
+        "flux": ["flux"],
+        "cutoff": ["ncut"],
+        "truncated_dim": ["truncated_dim"],
+    },
+    Fluxonium: {
+        "EJ": ["EJ"],
+        "EC": ["EC"],
+        "EL": ["EL"],
+        "flux": ["flux"],
+        "cutoff": ["ncut"],
+        "truncated_dim": ["truncated_dim"],
+    },
+    FluxQubit: {
+        "EJ": ["EJ1", "EJ2", "EJ3"],
+        "EC": ["ECJ1", "ECJ2", "ECJ3", "ECg1", "ECg2"],
+        "ng": ["ng1", "ng2"],
+        "flux": ["flux"],
+        "cutoff": ["ncut"],
+        "truncated_dim": ["truncated_dim"],
+    },
+    Cos2PhiQubit: {
+        "EJ": ["EJ"],
+        "EC": ["EC", "ECJ"],
+        "EL": ["EL"],
+        "disorder": ["dCJ", "dEJ", "dL"],
+        "flux": ["flux"],
+        "ng": ["ng"],
+        "cutoff": ["ncut", "phi_cut", "zeta_cut"],
+        "truncated_dim": ["truncated_dim"],
+    },
+    Oscillator: {
+        "E_osc": ["E_osc"],
+        "l_osc": ["l_osc"],
+        "truncated_dim": ["truncated_dim"],
+    },
+    KerrOscillator: {
+        "E_osc": ["E_osc"],
+        "l_osc": ["l_osc"],
+        "K": ["K"],
+        "truncated_dim": ["truncated_dim"],
+    },
+}
