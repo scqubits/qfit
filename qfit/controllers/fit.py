46ecd9e695c14eda468b5d7489faba4c21bbe0f2
Danyang Chen <dychen1999@foxmail.com>
2023-07-21 14:04:22 -0500

    change name & initial commit for the fit

---
+++ qfit/controllers/fit.py
@@ -0,0 +1,20 @@
+from qfit.controllers.wrapped_optimizer import Optimization, OptTraj
+
+class NumericalFitting():
+    def __init__(
+        self,
+    ):
+        pass
+
+    def onFixNFree(
+        self,
+    ):
+        # pass the fixed and free variables and update the optimization
+        pass
+
+    def runOptimization(
+        self,
+    ):
+        # once the user clicks the optimize button, run the optimization
+        pass
+
