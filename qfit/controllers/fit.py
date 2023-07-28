a229029ff13af1d72e58de4cad4d8eb3d0bfd4d6
Danyang Chen <dychen1999@foxmail.com>
2023-07-27 21:05:58 -0500

    connect fit in the mainwindow

--- qfit/controllers/fit.py
+++ qfit/controllers/fit.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Tuple, Callable, Union
 
 from qfit.controllers.wrapped_optimizer import Optimization, OptTraj
+from qfit.models.extracted_data import AllExtractedData
 from qfit.models.quantum_model_parameters import (
     QuantumModelFittingParameter,
     QuantumModelParameterSet,
@@ -12,27 +13,27 @@ class NumericalFitting():
     def __init__(
         self,
     ):
-        pass
+        return
 
     def setupUICallbacks(self):
         # set up callbacks for the UI
-        pass
+        return
 
     def _targetFunctionWrapper(
         self,
         paramDict: Dict[str, float],
         parameterSet: QuantumModelParameterSet,
         MSE: Callable,
-        # any other models needed when calculating the MSE
+        extractedData: AllExtractedData,
     ):
-        parameterSet.fromAttrDict(paramDict)
-        return MSE(parameterSet)
+        parameterSet.loadAttrDict(paramDict)
+        return MSE(parameterSet, extractedData)
     
-    def setUpObtimization(
+    def setupOptimization(
         self,
         parameterSet: QuantumModelParameterSet,
         MSE: Callable,
-        # any other models needed when fitting
+        extractedData: AllExtractedData,
     ):
         # generate the fixed parameters and free parameter ranges
         fixed_params = {}
@@ -53,6 +54,7 @@ class NumericalFitting():
             target_kwargs={
                 "parameterSet": parameterSet,
                 "MSE": MSE,
+                "extractedData": extractedData,
             },
         )
 
@@ -61,17 +63,19 @@ class NumericalFitting():
         freeParams: Dict[str, float],
         targetValue: float,
         parameterSet: QuantumModelParameterSet,
+        extractedData: AllExtractedData,
     ):
         # update the free parameters in the parameter set and display the target value
 
-        parameterSet.fromAttrDict(freeParams)
+        parameterSet.loadAttrDict(freeParams, "value")
 
-
+        print(targetValue)
 
         
     def runOptimization(
         self,
         parameterSet: QuantumModelParameterSet,
+        extractedData: AllExtractedData,
     ):
         """once the user clicks the optimize button, run the optimization"""
 
@@ -80,18 +84,23 @@ class NumericalFitting():
         
 
         # initial parameter
+        init_param_ui = parameterSet.exportAttrDict("initValue")
+        init_param = {key: value for key, value in init_param_ui.items() 
+                      if key not in self.opt.fixed_variables.keys()}
 
         try:
             traj = self.opt.run(
-                init_x = parameterSet.toAttrDict("initValue"),
+                init_x = init_param,
                 callback = self._optCallback,
-                callback_kwargs = {"parameterSet": parameterSet},
+                callback_kwargs = {
+                    "parameterSet": parameterSet,
+                    "extractedData": extractedData,
+                    },
                 # file_name = ...,
             )
         except AttributeError:
             # the optimization hasn't been set up yet
             pass
-
 
         # display the results, optimizer's termination message
         # enable all the line edits, sliders, etc
