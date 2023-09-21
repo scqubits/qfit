cdfb5ec457d1099f3989c9270e63e24f7dc1f4e8
Danyang Chen <dychen1999@foxmail.com>
2023-09-21 10:38:51 -0500

    auto translate in macos

---
+++ resources/auto_translate_ui.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Step 1: Run the terminal command to convert the UI file to Python code
+pyside6-uic ui_window.ui -o ui_window.py
+
+# Step 2: Change line 29 of ui_window.py
+sed -i '' '29s/import resources_rc/from . import resources_rc/' ui_window.py
+
+# Step 3: Move the ui_window.py to the folder ./../ui_designer/
+mv ui_window.py ./../qfit/ui_designer/
+
+echo "All tasks completed."
