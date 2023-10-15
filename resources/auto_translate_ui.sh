54b76a45d3e9bc0c50b3b3473da6df8cf6fa0151
Tianpu Zhao <pacosynthesis@gmail.com>
2023-10-15 14:44:02 -0500

    Recompiled UI menu

--- resources/auto_translate_ui.sh
+++ resources/auto_translate_ui.sh
@@ -5,6 +5,7 @@ pyside6-rcc -o resources_rc.py resources.qrc
 
 # Step 1: Run the terminal command to convert the UI file to Python code
 pyside6-uic ui_window.ui -o ui_window.py
+pyside6-uic ui_menu.ui -o ui_menu.py
 
 # Step 2: Change line 29 of ui_window.py
 sed -i '' '29s/import resources_rc/from . import resources_rc/' ui_window.py
@@ -12,5 +13,6 @@ sed -i '' '29s/import resources_rc/from . import resources_rc/' ui_window.py
 # Step 3: Move the ui_window.py to the folder ./../ui_designer/
 mv ui_window.py ./../qfit/ui_designer/
 mv resources_rc.py ./../qfit/ui_designer/
+mv ui_menu.py ./../qfit/ui_designer/
 
 echo "All tasks completed."
