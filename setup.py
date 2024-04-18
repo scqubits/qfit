9a68e846d3a1ba7c5caf812fd74681be41cf31aa
Danyang Chen <dychen1999@foxmail.com>
2024-04-18 15:50:09 -0500

    debug for action

--- setup.py
+++ setup.py
@@ -69,8 +69,12 @@ AUTHOR_EMAIL = (
     "jens-koch@northwestern.edu"
 )
 LICENSE = "BSD"
-DESCRIPTION = DOCLINES[0]
-LONG_DESCRIPTION = "\n".join(DOCLINES[2:])
+
+with open(os.path.join(CURDIR, 'README.md'), encoding='utf-8') as f:
+    README_CONTENT = f.read().split("\n")
+
+DESCRIPTION = README_CONTENT[0]
+LONG_DESCRIPTION = "\n".join(README_CONTENT[2:])
 KEYWORDS = "parameter extraction, superconducting qubits"
 
 URL = "https://github.com/scqubits/qfit"
@@ -95,15 +99,14 @@ if not ISRELEASED:
     FULLVERSION += ".dev" + str(MICRO) + git_short_hash()
 
 
-def write_version_py(filename="qfit/version.py"):
+def write_version_py():
     cnt = """\
 # THIS FILE IS GENERATED FROM qfit SETUP.PY
 short_version = '%(version)s'
 version = '%(fullversion)s'
 release = %(isrelease)s
 """
-    base_dir = os.path.abspath(os.path.dirname(__file__))  # Absolute path to your project directory
-    version_path = os.path.join(base_dir, 'qfit', 'version.py')
+    version_path = os.path.join(CURDIR, 'qfit', 'version.py')
 
     with open(version_path, "w") as versionfile:
         versionfile.write(
